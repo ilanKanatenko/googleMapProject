aeb14bf4434bd1953c482f3a8c4b52e09787f275
ilanKanatenko <andromyth@walla.com>
2021-02-03 03:31:21 +0200

    finish project that show your location on google maps and lets u change ur city if you want

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import TextField from "@material-ui/core/TextField";
 
 function App(props) {
-  console.log(props);
+  const [cityInfo, setCityName] = useState({
+    cityName: props.city,
+    lookForCity: props.city,
+  });
   return (
     <div>
-      <h1>asd</h1>
+      <TextField
+        disabled
+        defaultValue={props.IPv4}
+        label="your ip address"
+      ></TextField>
+      <TextField
+        onKeyPress={(event) => {
+          console.log(event.key);
+          event.key === "Enter" &&
+            setCityName({ ...cityInfo, lookForCity: cityInfo.cityName });
+          // setCityName({ ...cityInfo, cityName: event.target.value });
+
+          // event.key === "Enter" && setCityName("hadera");
+        }}
+        onChange={(event) => {
+          console.log(event);
+          setCityName({ ...cityInfo, cityName: event.target.value });
+
+          // event.key === "Enter" && setCityName("hadera");
+        }}
+        value={[cityInfo.cityName]}
+        label="City"
+      ></TextField>
+      <TextField
+        disabled
+        defaultValue={props.country_name}
+        label="Country"
+      ></TextField>
+      <br />
+      <br />
       <iframe
         width={"600"}
         height={"450"}
@@ -12,7 +45,7 @@ function App(props) {
         style={{ border: 0 }}
         src={
           "https://www.google.com/maps/embed/v1/search?q=" +
-          props.city +
+          cityInfo.lookForCity +
           "&key=AIzaSyCdQymwSuF0P6Ee-ffX0ZtWjpJdpaT5eLk"
         }
         allowFullScreen
--- src/index.js
+++ src/index.js
@@ -1,21 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom";
 // import "./index.css";
-import IPData from "ipdata";
+// import IPData from "ipdata";
 import App from "./components/App";
-let city = "hadera";
-const ipdata = new IPData(
-  "dcb19aafdd3accd0ca61b014f91a6cc5888d57f4f703a9758da3751e"
-);
 
-const ip = "109.64.100.27";
-ipdata.lookup(ip).then(function (data) {
-  city = data.city;
-  console.log(data.city);
-  ReactDOM.render(<App city={city} />, document.getElementById("root"));
-});
-// console.log(city);
+// const ipdata = new IPData(
+//   "dcb19aafdd3accd0ca61b014f91a6cc5888d57f4f703a9758da3751e"
+// );
 
-/* <React.StrictMode>
-    <App />
-  </React.StrictMode>, */
+//tried fetch on IPdata but it didnt work
+
+// fetch("https://api.ipdata.co")
+//   .then((response) => {
+//     return response.json();
+//   }, "jsonp")
+//   .then((res) => {
+//     console.log(res.ip);
+//   });
+
+//here if needed to use IPdata on known address
+
+// const ip = "109.64.100.27";
+// ipdata.lookup(ip).then(function (data) {
+//   city = data.city;
+//   console.log(data.city);
+//   ReactDOM.render(<App city={city} />, document.getElementById("root"));
+// });
+
+fetch("https://geolocation-db.com/json/")
+  .then((response) => response.json())
+  .then((data) => {
+    let { IPv4, city, country_name } = data;
+    console.log(city);
+    ReactDOM.render(
+      <App city={city} IPv4={IPv4} country_name={country_name} />,
+      document.getElementById("root")
+    );
+  });
